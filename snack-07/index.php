3ce922410f8b4c0f06f86c7da597f1528ad4e31d
federico697 <federico697697@gmail.com>
2022-12-14 16:24:51 +0100

    fine snack 07

--- snack-07/index.php
+++ snack-07/index.php
@@ -1,3 +1,28 @@
-<?php 
+<!-- Creare un array contenente qualche alunno di un’ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
 
+<?php 
+$arrayAlunni = [
+    [
+       'nome' => 'alex',
+       'cognome' => 'red',
+       'voti' => [2, 4, 8, 5, 10]
+    ],
+    [
+        'nome' => 'alessia',
+        'cognome' => 'verdi',
+        'voti' => [12, 6, 3, 9, 11]
+     ]
+];
+
+
+for( $i = 0; $i < count($arrayAlunni); $i++ ){
+
+    $mediaVoti = array_sum( $arrayAlunni[$i]['voti'] ) / count( $arrayAlunni[$i]['voti'] );
+
+    echo $arrayAlunni[$i]['nome'] . ' - ' . $arrayAlunni[$i]['cognome'] . ' ' . 'la media dei voti è : ' . $mediaVoti;
+    echo "<br>";
+
+}
 ?>
